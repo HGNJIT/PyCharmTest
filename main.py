f9a3dd77f3266c3a647072cf4c8c7b4fb438dbd4
Gil <heg2@njit.edu>
2021-01-25 09:03:27 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    print(f'hi, {name}')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == '__main__':
+    print_hi('IS218')
